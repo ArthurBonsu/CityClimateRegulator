54ed97d2af2ac85e19cbc347d1d8890067c4291c
ArthurBonsu <peeman34@gmail.com>
2023-09-07 03:00:42 +0800

    Update CityRegister.sol

--- CityRegister.sol
+++ CityRegister.sol
@@ -1,187 +1,111 @@
-
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
-import './StringUtils.sol';
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
+contract CityRegister is ERC20, Ownable {
 
-
-// Company to Company Setup 
-
-// City Register 
-//Company Register{name, address, nre device numbers, date of creation, company size, fuel}
-
-// Fuel emission per day 
-// carbon emission per day from fuel
-// nre emmission per day 
-// nre carbon emmission 
-// Company fuel = nre carbon + carbon emission per day
-
-// transfer fee tokens
-
-// Company Vault Parameter s
-
-
-// Company Register
-// 
-//Carbon Monitoring  Contract
-
-// City Carbon Details 
-// Calculate city temperature 
-// set city status 
-
-// Carbon Regulating Contract
-// compare parameters 
-
- //a) if temperature, humidity, wind, pressure < threshold  , 
- //b) increase parameters 
- 
-
-// Carbon Trading Contract 
-// a) Uniswap , Swap, Vault,Pair Token 
-// Token submit
-  // @dev contract for rock,paper game0
-contract CityRegister is    Ownable,ERC20 {
-    
-    
-
-    address public _owner;
-     string private _tokenname ="RPSTOKENS";
-    string private _tokensymbol= "RPS";
-    uint _payfee=0;
-         
-  
-       struct City  {
-             address  cityadderess;
-             string name;
-             string location;
-             uint256 longitude;
-             uint256 latitude;
-             uint256 carboncapacity;
-             uint256 amountpaid; 
-             uint256 citycountid;
-             
-             }
-
-  
-    uint256 public totalsupplytokens = 1000000;
-    mapping(address => address) registeredcities;
-    mapping(address => uint256) balances;
-
-    mapping(address=>City)public _citystore;
-   mapping(address => bool) registeredornot;
-     mapping(address => bool) paidcityescrowfee;
-      mapping(address => mapping(uint256 => uint256)) temperature;
-      mapping(address => mapping(uint256 => uint256)) carboncapacity;
-    mapping(address => mapping(uint256 => uint256)) humidity;
-  
-  // @dev objects inheriting from previous structs
-   City  newcity;   
-    City[] public newgamecity;
-      
-    
-      
-      constructor(address __owner) ERC20(_tokenname, _tokensymbol ) {
-        _owner =__owner;
-        
-         totalSupply();
-         
-
+  uint256 private payfee; 
+    struct City {
+        string name;
+        string location;
+        uint256 longitude;
+        uint256 latitude;
+        uint256 carbonCapacity;
+        uint256 amountPaid;
+        uint256 cityCountId;
     }
 
-  
+    mapping(address => City) public cityStore;
+    mapping(address => bool) public registeredCities;
+    mapping(address => bool) public paidCityEscrowFee;
 
-  // @dev fees to be paid to be registered for game 
-  // @params player=sender, contractowner,ether amount to be sent 
-   event  getCityParams(address cityaddress, uint256 carbonemmission, uint256 temperature, uint256 humidity,uint256 timeoftheday) ;
-  
-function payfee(address payable sender, address payable owneraddress, uint256 amount) public payable returns(bool, bytes memory){
-        
-         _owner =owneraddress;
-        _payfee = amount;
-     
-        require ( amount >= 10, "Amount not enough to play!");
-    
-          (bool success,bytes memory data ) = _owner.call{value: _payfee}("");
-            require(success, "Check the amount sent as well"); 
-             paidcityescrowfee[sender]= true;
-    return (success,data);
+    mapping(address => uint256) public carbonLevels;
+    mapping(address => uint256) public maxCreditLevels;
+      mapping(address => uint256) public carboncredit;
+
+    constructor() ERC20("RPSTOKENS", "RPS") {
+        _mint(msg.sender, 1000000 * 10 ** 18); // Mint 1,000,000 RPS tokens to the contract creator
     }
 
-   // @dev registeration for game  
-  // @params players name and address
+    function payFee(address payable cityAddress, uint256 amount) external payable {
+        require(amount >= 10 ether, "Amount must be at least 10 ether");
+        require(!paidCityEscrowFee[cityAddress], "Fee already paid for this city");
 
-   function getLatitude (uint256 _lat ) external  returns (uint256){
-   return  (_lat);
- }
+        (bool success, ) = cityAddress.call{value: amount}("");
+        require(success, "Payment failed");
 
- function getLongitude (uint256 _lng ) external  returns (uint256){
- return (_lng) ;
- }
-
- function getLocation (uint256 _lat, uint256 lng) public  returns (uint256,uint256){
-   return (_lat, lng) ;
- }
-
-  
-    function registerCity(string memory _cityname, address payable _cityaddress, uint256 _amount, uint256 _lng, uint256 _lat, uint256 _carboncapacity) public  returns(string memory, address){
-         // this means whoever is the owner is now the 
-         uint256 citycount = 0;
-              require(msg.sender == _owner , "Caller is not owner");
-              
-              payfee(_cityaddress, _owner,  _amount);
-         
-               if (paidcityescrowfee[_cityaddress] == true){
-            uint256 citybalance =0;
-            citybalance = balanceOf(_cityaddress);
-
-           if(registeredcities[_cityaddress] != _cityaddress){
-           
-            registeredcities[_cityaddress] =_cityaddress ;
-      (uint256 mylng, uint256 mylat )  = getLocation(_lng, _lat);
-         
-            // @dev storing to memory
-            newcity = City(_cityaddress,_cityname,location, _lng, _lat,  _carboncapacity, _amount,citycountid ); 
-             _citystore[_cityaddress].cityadderess = _cityaddress;
-             _citystore[_cityaddress].name = _cityname;
-             _citystore[_cityaddress].location = mylocation;
-             _citystore[_cityaddress].longitude = _lng;
-            _citystore[_cityaddress].latitude = _lat;
-            _citystore[_cityaddress].carboncapacity = carboncapacity;
-             _citystore[_cityaddress].amountpaid = _amount;
-             _citystore[_cityaddress].citycountid = citycount;
-                 // @dev storing to storage  -optional
-  
-  
-            newgamecity.push(newcity);                        
-
-            return (_cityaddress,_cityname,location, _lng, _lat,  _carboncapacity, _amount,citycountid  );
-     
-}
-}
+        paidCityEscrowFee[cityAddress] = true;
     }
 
+    function registerCity(
+        string memory cityName,
+        address payable cityAddress,
+        uint256 amount,
+        uint256 lng,
+        uint256 lat,
+        uint256 carbonCapacity
+    ) external onlyOwner {
+        require(!registeredCities[cityAddress], "City is already registered");
 
+        payFee(cityAddress, amount);
 
+        registeredCities[cityAddress] = true;
 
+        City storage newCity = cityStore[cityAddress];
+        newCity.name = cityName;
+        newCity.location = string(abi.encodePacked("Lat: ", lat, ", Lng: ", lng));
+        newCity.longitude = lng;
+        newCity.latitude = lat;
+        newCity.carbonCapacity = carbonCapacity;
+        newCity.amountPaid = amount;
+        newCity.cityCountId = totalSupply(); // Assign an ID based on total supply
 
-function getCityParameters (address cityaddress, uint256 carbonemmission, uint256 temperature, uint256 humidity) external returns (uint256 _carbonemmission, uint256 _temperature, uint256 _humidity, uint256 _timeoftheday) {
-     uint256 timeoftheday = block.timestamp; 
+        _mint(cityAddress, amount); // Mint tokens to the registered city
+    }
 
-     temperature[cityaddress][timeoftheday] =temperature;
-     carboncapacity[cityaddress][timeoftheday] = carbonemmission;
-    carboncapacity[cityaddress][timeoftheday] = humidity;
-     
- emit getCityParams( cityaddress, carbonemmission,  temperature, humidity, timeoftheday) ;
-     
-  return (carbonemmission,  temperature,  humidity,timeoftheday);
+    function getCityParameters(
+        address cityAddress,
+        uint256 carbonEmission,
+        uint256 temp,
+        uint256 humidity
+    ) external onlyOwner {
+        uint256 timeOfDay = block.timestamp;
+
+        // Store city parameters
+        cityStore[cityAddress].carbonCapacity = carbonEmission;
+        cityStore[cityAddress].longitude = temp;
+        cityStore[cityAddress].latitude = humidity;
+        carbonLevels[cityAddress] = carbonEmission;
+
+        emit GetCityParams(cityAddress, carbonEmission, temp, humidity, timeOfDay);
+    }
+
+    event GetCityParams(
+        address indexed cityAddress,
+        uint256 carbonEmission,
+        uint256 temp,
+        uint256 humidity,
+        uint256 timeOfDay
+    );
+
+    function setMaximumCarbonLevel(address cityAddress, uint256 maximumCarbonLevel) external returns (address, uint256) {
+        // Check if the carbon level for the city is not set (initialized to 0)
+        if (carbonLevels[cityAddress] == 0) {
+            maxCreditLevels[cityAddress] = maximumCarbonLevel;
+        }
+
+        return (cityAddress, maximumCarbonLevel);
+    }
+
+    function getCarbonLevel(address cityAddress) external view returns (uint256) {
+        return carbonLevels[cityAddress];
+    }
+
+    function getCarbonCredit(address cityAddress) external view returns (uint256) {
+        uint256 carbonCredit = maxCreditLevels[cityAddress] - carbonLevels[cityAddress];
+        carboncredit[cityAddress] = carbonCredit; 
+        return carbonCredit;
+    }
 }
-        
-
- }
-    
-      // @dev register game, game id as random generator integer
-   
-
