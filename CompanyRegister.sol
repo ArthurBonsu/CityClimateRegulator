b427f8ca80dae529f49ffef406599a33c72877ff
ArthurBonsu <peeman34@gmail.com>
2023-09-07 03:01:00 +0800

    Update CompanyRegister.sol

--- CompanyRegister.sol
+++ CompanyRegister.sol
@@ -1,191 +1,153 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
-import './StringUtils.sol';
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "contracts/CityRegister.sol";
 
-
-
-// Company to Company Setup 
-
-// company Register 
-//Company Register{name, address, nre device numbers, date of creation, company size, fuel}
-
-// Fuel emission per day 
-// carbon emission per day from fuel
-// nre emmission per day 
-// nre carbon emmission 
-// Company fuel = nre carbon + carbon emission per day
-
-// transfer fee tokens
-
-// Company Vault Parameter s
-
-
-// Company Register
-// 
-//Carbon Monitoring  Contract
-
-// company Carbon Details 
-// Calculate company temperature 
-// set company status 
-
-// Carbon Regulating Contract
-// compare parameters 
-
- //a) if temperature, humidity, wind, pressure < threshold  , 
- //b) increase parameters 
- 
-
-// Carbon Trading Contract 
-// a) Uniswap , Swap, Vault,Pair Token 
-// Token submit
-  // @dev contract for rock,paper game0
-contract CompanyRegister is    Ownable,ERC20 {
-    
-    
-
+contract CompanyRegister is CityRegister, ERC20, Ownable {
     address public _owner;
-     string private _tokenname ="RPSTOKENS";
-    string private _tokensymbol= "RPS";
-    uint _payfee=0;
-         
-  
-       struct Company  {
-             address  companyadderess;
-             address city; 
-             string name;
-             string location;
-             uint256 longitude;
-             uint256 latitude;
-             uint256 _carboncapacity;
-             uint256 amountpaid; 
-             uint256 companycountid;
-             
-             }
+    string private _tokenName = "RPSTOKENS";
+    string private _tokenSymbol = "RPS";
+    uint256 private _payFee = 0;
 
-  
-    uint256 public totalsupplytokens = 1000000;
-    mapping(address => address) registeredcompanies;
-    mapping(address => uint256) balances;
-
-    mapping(address=>Company)public _companystore;
-   mapping(address => bool) registeredornot;
-     mapping(address => bool) paidcompanyescrowfee;
-
-     mapping(address => mapping(address => address)) companyincity;
-         mapping(address => mapping(address => bool)) checkifcompanyisincity;
-      mapping(address => mapping(uint256 => uint256)) temperature;
-      mapping(address => mapping(uint256 => uint256)) carboncapacity;
-    mapping(address => mapping(uint256 => uint256)) humidity;
-  
-  // @dev objects inheriting from previous structs
-   Company  newcompany;   
-    Company[] public newgamecompany;
-      
-    
-      
-      constructor(address __owner) ERC20(_tokenname, _tokensymbol ) {
-        _owner =__owner;
-        
-         totalSupply();
-         
-
+    struct Company {
+        address companyAddress;
+        address city;
+        string name;
+        string location;
+        uint256 longitude;
+        uint256 latitude;
+        uint256 carbonCapacity;
+        uint256 amountPaid;
+        uint256 companyCountId;
     }
 
-  
+    uint256 public totalSupplyTokens = 1000000;
 
-  // @dev fees to be paid to be registered for game 
-  // @params player=sender, contractowner,ether amount to be sent 
-   event  getcompanyParams(address city, address companyaddress, uint256 carbonemmission, uint256 temperature, uint256 humidity,uint256 timeoftheday) ;
-  
-function payfee(address payable sender, address payable owneraddress, uint256 amount) public payable returns(bool, bytes memory){
-        
-         _owner =owneraddress;
-        _payfee = amount;
-     
-        require ( amount >= 10, "Amount not enough to play!");
-    
-          (bool success,bytes memory data ) = _owner.call{value: _payfee}("");
-            require(success, "Check the amount sent as well"); 
-             paidcompanyescrowfee[sender]= true;
-    return (success,data);
+    mapping(address => bool) public registeredCompanies;
+    mapping(address => bool) public paidCompanyEscrowFee;
+    mapping(address => mapping(address => bool)) public checkIfCompanyIsInCity;
+
+    mapping(address => Company) public companyStore;
+    mapping(address => mapping(uint256 => uint256)) public temperature;
+    mapping(address => mapping(uint256 => uint256)) public carbonCapacityCompany;
+    mapping(address => mapping(uint256 => uint256)) public humidity;
+
+
+ /// checkimg carbon level of companiesw
+
+ mapping(address => uint256) public companycarbonLevels;
+    mapping(address => uint256) public companymaxCreditLevels;
+      mapping(address => uint256) public companycarboncredit;
+
+    Company[] public newCompany;
+
+    event GetCompanyParams(
+        address indexed city,
+        address indexed companyAddress,
+        uint256 carbonEmission,
+        uint256 temperature,
+        uint256 humidity,
+        uint256 timeOfDay
+    );
+
+    constructor(address ownerAddress) ERC20(_tokenName, _tokenSymbol) {
+        _owner = ownerAddress;
+        totalSupply();
     }
 
-   // @dev registeration for game  
-  // @params players name and address
-  
-    function registercompany(string memory _companyname, address payable _companyaddress,address _cityaddress,  uint256 _amount, uint256 _lng, uint256 _lat, uint256 _carboncapacity) external  returns(address ,address , string ,uint256 , uint256 , uint256 ,  uint256,uint256 ,uint256 ){
-         // this means whoever is the owner is now the 
-         uint256 companycount = 0;
-              require(msg.sender == _owner , "Caller is not owner");
-              
-              payfee(_companyaddress, _owner,  _amount);
-                   if(checkifcompanyisincity[_cityaddress][_companyaddress] = false) {
-               if (paidcompanyescrowfee[_companyaddress] == true){
-            uint256 companybalance =0;
-            companybalance = balanceOf(_companyaddress);
+    function payFees(address payable sender, address payable ownerAddress, uint256 amount) external payable {
+        _owner = ownerAddress;
+        _payFee = amount;
+        require(amount >= 10 ether, "Amount must be at least 10 ether");
 
-           if(registeredcompanies[_companyaddress] != _companyaddress){
-           
-            registeredcompanies[_companyaddress] =_companyaddress ;
-     uint256 mylocation = getLocation(_lng, _lat);
+        (bool success, ) = _owner.call{value: _payFee}("");
+        require(success, "Payment failed");
 
-           companyincity[_cityaddress][_companyaddress] =_companyaddress;
-           checkifcompanyisincity[_cityaddress][_companyaddress] = true;
-            // @dev storing to memory
-            newcompany = company(_companyaddress,_cityaddress, _companyname,location, _lng, _lat,  _carboncapacity, _amount,companycountid ); 
-             _companystore[_companyaddress].companyadderess = _companyaddress;
-               _companystore[_companyaddress].city = _cityaddress;
-             _companystore[_companyaddress].name = _companyname;
-             _companystore[_companyaddress].location = mylocation;
-             _companystore[_companyaddress].longitude = _lng;
-            _companystore[_companyaddress].latitude = _lat;
-            _companystore[_companyaddress].carboncapacity = carboncapacity;
-             _companystore[_companyaddress].amountpaid = _amount;
-             _companystore[_companyaddress].companycountid = companycount;
-                 // @dev storing to storage  -optional
-  
-  
-            newgamecompany.push(newcompany);                        
-           }
-            return (__companyaddress, _cityaddress,  _companyname, location,  _lng, _lat,  _carboncapacity, _amount, companycountid  );
-     
-}
-}
+        paidCompanyEscrowFee[sender] = true;
     }
 
+    function registerCompany(
+        string memory companyName,
+        address payable companyAddress,
+        address cityAddress,
+        uint256 amount,
+        uint256 lng,
+        uint256 lat,
+        uint256 carbonCapacity
+    ) external onlyOwner returns (address, string memory, uint256, uint256, uint256, uint256, uint256, uint256) {
+        uint256 companyCount = 0;
+        require(msg.sender == _owner, "Caller is not the owner");
 
- function getLatitude (uint256 _lat ) external  returns (uint finallat){
-   return  (_lat);
- }
+        payFees(companyAddress, _owner, amount);
 
- function getLongitude (uint256 _lng ) external  returns (uint finallng){
- return (_lng) ;
- }
+        if (!checkIfCompanyIsInCity[cityAddress][companyAddress]) {
+            if (paidCompanyEscrowFee[companyAddress]) {
+                if (!registeredCompanies[companyAddress]) {
+                    registeredCompanies[companyAddress] = true;
 
- function getLocation (uint256 _lat, uint256 lng) external  returns (uint256,uint256   ){
-   return (_lat, _lng) ;
- }
+                    uint256 myLocation = getLocation(lng, lat);
 
+                    companyStore[companyAddress] = Company({
+                        companyAddress: companyAddress,
+                        city: cityAddress,
+                        name: companyName,
+                        location: myLocation,
+                        longitude: lng,
+                        latitude: lat,
+                        carbonCapacity: carbonCapacity,
+                        amountPaid: amount,
+                        companyCountId: companyCount
+                    });
 
+                    checkIfCompanyIsInCity[cityAddress][companyAddress] = true;
 
-function getcompanyParameters (address city, address companyaddress, uint256 carbonemmission, uint256 temperature, uint256 humidity) external returns (address, uint256, uint256, uint256, uint256) {
-     uint256 timeoftheday = block.timestamp; 
+                    newCompany.push(companyStore[companyAddress]);
+                }
+            }
+        }
 
-     temperature[companyaddress][timeoftheday] =temperature;
-     carboncapacompany[companyaddress][timeoftheday] = carbonemmission;
-    carboncapacompany[companyaddress][timeoftheday] = humidity;
-     
- emit getcompanyParams(city,  companyaddress, carbonemmission,  temperature, humidity, timeoftheday) ;
-     
-  return (city, companyaddress, carbonemmission,  temperature,  humidity,timeoftheday);
+        return (
+            companyAddress,
+            companyName,
+            amount,
+            lng,
+            lat,
+            carbonCapacity,
+            amount,
+            companyCount
+        );
+    }
+
+    function getLatitude(uint256 lat) external pure returns (uint256) {
+        return lat;
+    }
+
+    function getLongitude(uint256 lng) external pure returns (uint256) {
+        return lng;
+    }
+
+    function getLocation(uint256 lat, uint256 lng) internal pure returns (string memory) {
+        return string(abi.encodePacked("Lat: ", lat, ", Lng: ", lng));
+    }
+
+    function getCompanyParameters(
+        address city,
+        address companyAddress,
+        uint256 carbonEmission,
+        uint256 temp,
+        uint256 humidity
+    ) external onlyOwner returns (address, uint256, uint256, uint256, uint256) {
+        uint256 timeOfDay = block.timestamp;
+
+        temperature[companyAddress][timeOfDay] = temp;
+        carbonCapacityCompany[companyAddress][timeOfDay] = carbonEmission;
+        humidity[companyAddress][timeOfDay] = humidity;
+
+        emit GetCompanyParams(city, companyAddress, carbonEmission, temp, humidity, timeOfDay);
+
+        return (city, companyAddress, carbonEmission, temp, humidity);
+    }
+
 }
-        
-
- }
-    
-      // @dev register game, game id as random generator integer
-   
-
